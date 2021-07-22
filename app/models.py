13a33d49c6ed058349586c41d99b2f0f848a87ea
xh4vm <h4vm@yandex.ru>
2021-07-22 12:22:28 +0500

    Структура базы данных

--- app/models.py
+++ app/models.py
@@ -1,26 +1,30 @@
 from app import db, jwt
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+import re
 
-#commit right work check
+
 class User(db.Model):
     __tablename__ = "user"
+    __table_args__ = (
+        db.CheckConstraint(re.match('[^@]+@[^\.]+[^$]+', email, re.IGNORECASE)),
+    )
 
     id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(128), nullable=False)
-    first_name = db.Column(db.String(128), nullable=False, default='')
-    last_name = db.Column(db.String(128), nullable=False, default='')
-    email = db.Column(db.String(128), nullable=False)
+    nickname = db.Column(db.String(128), nullable=False, unique=True)
+    first_name = db.Column(db.String(128), nullable=True)
+    last_name = db.Column(db.String(128), nullable=True)
+    email = db.Column(db.String(128), nullable=False, unique=True)
     password = db.Column(db.String(128), nullable=False)
-    image = db.Column(db.String(128))
+    avatar = db.Column(db.String(128), nullable=True)
 
-    def __init__(self, nickname, email, password, image=None, first_name=None, last_name=None):
+    def __init__(self, nickname, email, password, avatar=None, first_name=None, last_name=None):
         self.nickname = nickname
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.password = generate_password_hash(password)
-        self.image = image
+        self.avatar = avatar
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
@@ -35,11 +39,12 @@ class User(db.Model):
         return {
             'email': user.email,
             'nickname': user.nickname,
-            'avatar': user.image
+            'avatar': user.avatar,
+            'first_name': user.first_name,
+            'last_name': user.last_name
         }
 
     @staticmethod
     @jwt.user_identity_loader
     def add_identity(user):
         return user.id
-
