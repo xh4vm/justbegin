e7610c4374914494b8b11fce247e26df86e78c5e
xh4vm <h4vm@yandex.ru>
2021-09-01 00:02:47 +0500

    Update Home

--- app/home/routes.py
+++ app/home/routes.py
@@ -1,15 +1,6 @@
-from flask import render_template, request, jsonify, redirect
-from flask_jwt_extended import get_jwt_claims, jwt_optional, get_jwt_identity, get_raw_jwt
-from flask_classy import FlaskView, route
-from app.home import bp
-from .exceptions import HomeExceptions
+from flask_classy import FlaskView
 
 
 class Home(FlaskView):
     pass
 
-@bp.route('/', methods=['GET'])
-def index_view():
-    return redirect('/home/', code=303)
-
-Home.register(bp)
